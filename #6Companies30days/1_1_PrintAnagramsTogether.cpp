676dc58fdc208073956d2be1c4509aaf05c47c96
Deepali Thakur <77734636+deepalii05@users.noreply.github.com>
2022-01-09 15:22:09 +0530

    Add files via upload

---
+++ #6Companies30days/1_1_PrintAnagramsTogether.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        vector<vector<string>> Ans;
+        string str;
+        unordered_map<string,vector<string>>Map;
+        for(int i=0;i<string_list.size();i++){
+            str=string_list[i];
+            sort(string_list[i].begin(),string_list[i].end());
+            Map[string_list[i]].push_back(str);
+        }
+        for(auto i:Map){
+            Ans.push_back(i.second);
+        }
+        
+        return Ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
